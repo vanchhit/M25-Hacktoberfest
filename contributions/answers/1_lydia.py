0db78ffd8a68d2c1cbe064c223c4c257ae0132f2
lyds-jane <52633187+lyds-jane@users.noreply.github.com>
2021-10-15 17:27:24 -0700

    Create 1_lydia.py

---
+++ contributions/answers/1_lydia.py
@@ -0,0 +1,17 @@
+def leap_year_checker(int(year)):
+  #To be a leap year, the year number must be divisible by four – except for end-of-century years, which must be divisible by 400
+  #source: https://thatascience.com/learn-python/comments-and-docstrings/
+  
+  #if it is not the end of a century...
+  if str(year[:2]) != "00": 
+    if year % 4 == 0: #...and it is divisible by 4
+      return True
+    else:
+      return False #...and it is not divisible by 4
+  
+  #if it is the end of a century...
+  else: 
+    if year % 400 == 0: #...and it is divisible by 400
+      return True
+    else: #...and it is not divisible by 400
+      return False
