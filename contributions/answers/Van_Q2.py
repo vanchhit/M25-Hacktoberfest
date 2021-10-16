53c78d8de9ecc01d06604c7758015b4f3ee8a1b7
Vanchhit Sahal <52569534+vanchhit@users.noreply.github.com>
2021-10-15 20:45:05 -0700

    Create Van_Q2.py
    Answers Question 2, with different orders.

---
+++ contributions/answers/Van_Q2.py
@@ -0,0 +1,41 @@
+# This code answers any prompt from question 2.
+
+def result(c):
+  key = {
+    'C' : 0,
+    'E' : 1,
+    'G' : 2,
+    'I' : 3,
+    'K' : 4,
+    'M' : 5,
+    'O' : 6,
+    'Q' : 7,
+    'S' : 8,
+    'U' : 9,
+    '!' : 1000,
+    '%' : 100,
+    '*' : 10,
+    '#' : 1,
+  }
+  
+  return key.get(c)
+
+
+string = input("Enter the crypted prompt: ")
+
+
+ans = 0
+
+i = 0
+while i<len(string):
+  while(string[i] == " "):
+    i+=1
+  temp = result(string[i])
+  i+=1
+  while(string[i] == " "):
+    i+=1
+  ans += (temp*result(string[i]))
+  i+=1
+  
+print(ans)
+   
